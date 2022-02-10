eb024090e340a9d526431d4de7ff4ac0c9288a25
hrdrok <hrdrok26@gmail.com>
2022-02-10 14:23:08 -0500

    multipager2

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
             <h1 class="font-weight-bold pt-5">
               Hafgufa, Krake, Sciu-crak, and Kraken, it goes by many names. It has taken many forms, but all accounts of the legendary Kraken come to the same conclusion.
               It's waiting around to feast on the weak, traveling its' waters. In crypto, whales are the top of the foodchain, and here we give everyone a chance to be one themselves.
-              But not all is as it seems. Something about these whales are different. They're changing...evolving...or perhaps mutating. Perhaps the olde tales of the Kraken weren't fables.
+              But not all is as it seems. Something about these whales are different. They're changing...evolving...or possibly mutating. Perhaps the olde tales of the Kraken weren't fables.
               Maybe, just maybe, the era of Krakens is returning. 
             </h1>
             <div class= "container-fluid pt-5"></div>
--- src/Roadmap.jsx
+++ src/Roadmap.jsx
@@ -11,22 +11,22 @@ function Roadmap() {
           <div class="col-lg-12">
             <img
               class="img-thumbnail text-center"
-              src="https://imgur.com/EINgLYN.png"
+              src="https://imgur.com/W7wOyWF.png"
               alt=""
             />
           </div>
           <div class= "container-fluid pt-5"></div>
           <div class="col-lg-12">
             <h1 class="display-3 font-weight-light pt-5">Roadmap</h1>
-            <p class="font-weight- bold pt-5">
-              Coming Soon!
+            <p class="display-4 font-weight-bold pt-5">
+              Coming Soon! We have big plans for Cracken Krakens. Stay tuned for updates!
             </p>
             <div class= "container-fluid pt-5"></div>
             <div class= "container-fluid pt-5"></div>
             <div class= "container-fluid pt-5"></div>
             <div class= "container-fluid pt-5"></div>
             <div class= "container-fluid pt-5"></div>
-            <div class= "container-fluid pt-5"></div>            
+                      
           </div>
         </div>
       </div>
